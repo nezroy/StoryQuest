208899b8da74fe07dc7905200dfe375e93631bae
nezroy <github@nezroy.com>
2025-11-19 14:07:39 -0700

    Add Midnight backgrounds

--- data/MapData.lua
+++ data/MapData.lua
@@ -162,6 +162,9 @@ local M = {
     [2367] = "TWW/vault_of_memory",
     [2371] = "TWW/karesh",
     [2472] = "TWW/tazavesh",
+    [2393] = "Midnight/silvermoon_city",
+    [2424] = "Midnight/queldanas",
+    [2432] = "Midnight/queldanas_intro",
 }
 PKG.MAP_BGS = M
 
